fef9c44bdc7eb5f04320ca1f458ce94362f5dd05
RedPlayer_1 <100540718+RedPlayer1@users.noreply.github.com>
2024-02-19 13:50:10 -0500

    initial commit

---
+++ src/main/java/me/redplayer_1/towerdefense/TowerDefense.java
@@ -0,0 +1,17 @@
+package me.redplayer_1.towerdefense;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TowerDefense extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
